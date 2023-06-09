47e189eef141644012daee9f53aede7aeb217744
wjyalone <340508578@qq.com>
2023-06-09 21:51:57 +0800

    6x9

---
+++ shipgame/main.py
@@ -0,0 +1,19 @@
+import pygame
+import sys
+from settings import Settings
+
+
+def run_game():
+    pygame.init()
+    settings = Settings()
+    screen = pygame.display.set_mode(settings.screen_size)
+    pygame.display.set_caption("test")
+    while True:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+        
+        screen.fill(settings.bg_color)
+        pygame.display.flip()
+
+run_game()
---
+++ shipgame/settings.py
@@ -0,0 +1,5 @@
+class Settings():
+    
+    def __init__(self) -> None:
+        self.screen_size = (500, 700)
+        self.bg_color = (230,230,230)
