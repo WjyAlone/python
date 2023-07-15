190fa45142aadbb02a87e76d8126ddc5e66a1926
WjyAlone <340508578@qq.com>
2023-07-15 21:11:28 +0800

    to

---
+++ Mathtics/Toline.py
@@ -0,0 +1,10 @@
+k = 1
+point = [(1, 2), (2, 5), (5, 7), (10, 15)]
+def toNient(pio, formulaK):
+    result = 0
+    for i in pio:
+        result += (i[1]-formulaK * i[0])
+    return result
+for i in range(10):
+    k += toNient(point, k) * 0.1
+print(k)
--- spider/rr.py
+++ spider/rr.py
@@ -1,2 +1,12 @@
-for i in range(1,5):
-    print(i)
+import requests
+
+for i in range(1, 1000):
+    url = "https://vip.aqdw"+str(i)+".com/videos/tag/cartoon"
+    try:
+        resss = requests.get(url, timeout=0.5)
+    except:
+        pass
+    else:
+        if resss.status_code == 200:
+            print(f"URL:HTTPS_{i}", "CONNECT SUCCESSFULLY")
+
