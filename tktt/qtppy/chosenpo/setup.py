0c65fc5b184a2b6ae0b82f1beeb1f1c42cc78009
easyFrog <117511670+WjyAlone@users.noreply.github.com>
2023-07-23 02:30:02 +0000

    mm

--- tktt/qtppy/chosenpo/setup.py
+++ tktt/qtppy/chosenpo/setup.py
@@ -1,3 +1,17 @@
+import typing
 import sys
-from PyQt5.QtWidgets import QMainWindow, QApplication, QWidget
+from PyQt5 import QtCore
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
+from Ui_untitled import Ui_aa
 
+class mainmau(QMainWindow, Ui_aa):
+    def __init__(self, parent = None) -> None:
+        super(mainmau, self).__init__(parent)
+        self.setupUi(self)
+        
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    mywin = mainmau()
+    mywin.show()
+    sys.exit(app.exec_())
+    
