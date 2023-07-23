30417f8f331616826fb288a0ddbca31ce0518ace
WjyAlone <340508578@qq.com>
2023-07-23 10:50:05 +0800

    ss

--- tktt/qtppy/chosenpo/Ui_untitled.py
+++ tktt/qtppy/chosenpo/Ui_untitled.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'd:\Kingf\tktt\qtppy\untitled.ui'
+# Form implementation generated from reading ui file 'd:\Kingf\tktt\qtppy\chosenpo\untitled.ui'
 #
 # Created by: PyQt5 UI code generator 5.15.9
 #
@@ -11,58 +11,66 @@
 from PyQt5 import QtCore, QtGui, QtWidgets
 
 
-class Ui_aa(object):
-    def setupUi(self, aa):
-        aa.setObjectName("aa")
-        aa.resize(384, 251)
-        self.centralwidget = QtWidgets.QWidget(aa)
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        MainWindow.setObjectName("MainWindow")
+        MainWindow.resize(384, 251)
+        self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         self.textBrowser = QtWidgets.QTextBrowser(self.centralwidget)
-        self.textBrowser.setGeometry(QtCore.QRect(40, 50, 91, 121))
+        self.textBrowser.setGeometry(QtCore.QRect(30, 40, 111, 131))
         self.textBrowser.setObjectName("textBrowser")
         self.lineEdit = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit.setGeometry(QtCore.QRect(220, 190, 51, 20))
+        self.lineEdit.setGeometry(QtCore.QRect(220, 200, 51, 20))
         self.lineEdit.setObjectName("lineEdit")
         self.label = QtWidgets.QLabel(self.centralwidget)
-        self.label.setGeometry(QtCore.QRect(280, 190, 31, 16))
+        self.label.setGeometry(QtCore.QRect(280, 200, 31, 16))
         self.label.setObjectName("label")
         self.pushButton = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton.setGeometry(QtCore.QRect(210, 150, 91, 31))
+        self.pushButton.setGeometry(QtCore.QRect(200, 160, 91, 31))
         self.pushButton.setObjectName("pushButton")
         self.pushButton_2 = QtWidgets.QPushButton(self.centralwidget)
-        self.pushButton_2.setGeometry(QtCore.QRect(40, 180, 81, 31))
+        self.pushButton_2.setGeometry(QtCore.QRect(50, 190, 81, 31))
         self.pushButton_2.setObjectName("pushButton_2")
         self.label_2 = QtWidgets.QLabel(self.centralwidget)
         self.label_2.setGeometry(QtCore.QRect(310, 230, 71, 21))
         self.label_2.setObjectName("label_2")
         self.lineEdit_2 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_2.setGeometry(QtCore.QRect(190, 90, 41, 31))
+        self.lineEdit_2.setGeometry(QtCore.QRect(190, 120, 41, 31))
         self.lineEdit_2.setObjectName("lineEdit_2")
         self.lineEdit_3 = QtWidgets.QLineEdit(self.centralwidget)
-        self.lineEdit_3.setGeometry(QtCore.QRect(270, 90, 41, 31))
+        self.lineEdit_3.setGeometry(QtCore.QRect(260, 120, 41, 31))
         self.lineEdit_3.setObjectName("lineEdit_3")
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
-        self.label_3.setGeometry(QtCore.QRect(250, 100, 16, 16))
+        self.label_3.setGeometry(QtCore.QRect(240, 130, 16, 16))
         self.label_3.setObjectName("label_3")
-        aa.setCentralWidget(self.centralwidget)
+        self.textBrowser_2 = QtWidgets.QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setGeometry(QtCore.QRect(200, 20, 101, 91))
+        self.textBrowser_2.setObjectName("textBrowser_2")
+        MainWindow.setCentralWidget(self.centralwidget)
 
-        self.retranslateUi(aa)
-        QtCore.QMetaObject.connectSlotsByName(aa)
+        self.retranslateUi(MainWindow)
+        QtCore.QMetaObject.connectSlotsByName(MainWindow)
 
-    def retranslateUi(self, aa):
+    def retranslateUi(self, MainWindow):
         _translate = QtCore.QCoreApplication.translate
-        aa.setWindowTitle(_translate("aa", "MainWindow"))
-        self.textBrowser.setHtml(_translate("aa", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+        MainWindow.setWindowTitle(_translate("MainWindow", "学号抽取器"))
+        self.textBrowser.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
 "<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
 "p, li { white-space: pre-wrap; }\n"
 "</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
 "<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:72pt;\">0</span></p></body></html>"))
-        self.lineEdit.setText(_translate("aa", "200"))
-        self.label.setText(_translate("aa", "毫秒"))
-        self.pushButton.setText(_translate("aa", "滚动抽取"))
-        self.pushButton_2.setText(_translate("aa", "随机抽取"))
-        self.label_2.setText(_translate("aa", "Made by cJkl"))
-        self.lineEdit_2.setToolTip(_translate("aa", "<html><head/><body><p>5555</p></body></html>"))
-        self.lineEdit_2.setText(_translate("aa", "1"))
-        self.lineEdit_3.setText(_translate("aa", "25"))
-        self.label_3.setText(_translate("aa", "~"))
+        self.lineEdit.setText(_translate("MainWindow", "200"))
+        self.label.setText(_translate("MainWindow", "毫秒"))
+        self.pushButton.setText(_translate("MainWindow", "滚动抽取"))
+        self.pushButton_2.setText(_translate("MainWindow", "随机抽取"))
+        self.label_2.setText(_translate("MainWindow", "Made by cJkl"))
+        self.lineEdit_2.setToolTip(_translate("MainWindow", "<html><head/><body><p>5555</p></body></html>"))
+        self.lineEdit_2.setText(_translate("MainWindow", "1"))
+        self.lineEdit_3.setText(_translate("MainWindow", "25"))
+        self.label_3.setText(_translate("MainWindow", "~"))
+        self.textBrowser_2.setHtml(_translate("MainWindow", "<!DOCTYPE HTML PUBLIC \"-//W3C//DTD HTML 4.0//EN\" \"http://www.w3.org/TR/REC-html40/strict.dtd\">\n"
+"<html><head><meta name=\"qrichtext\" content=\"1\" /><style type=\"text/css\">\n"
+"p, li { white-space: pre-wrap; }\n"
+"</style></head><body style=\" font-family:\'SimSun\'; font-size:9pt; font-weight:400; font-style:normal;\">\n"
+"<p align=\"center\" style=\" margin-top:0px; margin-bottom:0px; margin-left:0px; margin-right:0px; -qt-block-indent:0; text-indent:0px;\"><span style=\" font-size:48pt;\">1</span></p></body></html>"))
--- tktt/qtppy/chosenpo/setup.py
+++ tktt/qtppy/chosenpo/setup.py
@@ -1,10 +1,8 @@
-import typing
 import sys
-from PyQt5 import QtCore
 from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
-from Ui_untitled import Ui_aa
+from Ui_untitled import Ui_MainWindow
 
-class mainmau(QMainWindow, Ui_aa):
+class mainmau(QMainWindow, Ui_MainWindow):
     def __init__(self, parent = None) -> None:
         super(mainmau, self).__init__(parent)
         self.setupUi(self)
