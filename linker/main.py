c10b720387b8ad71014cdf0729a202964ef10712
WjyAlone <340508578@qq.com>
2023-07-25 20:31:14 +0800

    now

--- linker/main.py
+++ linker/main.py
@@ -1,17 +1,42 @@
-from flask import Flask, request
-
+from flask import Flask, request, template_rendered
+import json
+from time import sleep
 app = Flask(__name__)
-
 @app.route('/', methods = ['POST', 'GET'])
 def accept():
     if request.method == 'POST':
-        result = request.data
-        print(str(result, 'utf-8'))
-        with open('result.txt', 'w') as f:
-            f.write(str(result, 'utf-8'))
-        return result
-    else:
-        return 'NO POST'
+        addtt = False
+        resultjs = json.loads(str(request.data, encoding='utf-8'))
+        with open('result.json', 'r') as f:
+            textjs = json.loads(f.read())
+            if len(textjs) != 0:
+                for i in range(len(textjs)):
+                    if resultjs["name"] == textjs[i]["name"]:
+                        with open('result.json', 'w') as a:
+                            textjs[i]["location"] = resultjs["location"]
+                            a.write(json.dumps(textjs))
+                            addtt = False
+                        break
+                    else:
+                        
+                        addtt = True
+            else:
+                textjs.append(resultjs)
+                with open('result.json', 'w') as a:
+                    a.write(json.dumps(textjs))
+                    addtt = False
+            if addtt:
+                
+                textjs.append(resultjs)
+                with open('result.json', 'w') as a:
+                    a.write(json.dumps(textjs))
+                    addtt = False
+        with open('result.json', 'r') as t:
+            return t.read()
+    elif request.method == "GET":
+        with open('result.json', 'r') as t:
+            return t.read()
+    
 
 if __name__ == '__main__':
     app.run(port=25560, debug=True)
--- linker/user.py
+++ linker/user.py
@@ -1,5 +1,21 @@
 import requests
+import json
+from time import sleep
+from tkinter import Tk
+name = 'wjy'
+diction = {}
+url = 'https://apis.map.qq.com/ws/location/v1/ip?key=53VBZ-AFAWG-ZCVQ4-QQUUO-26CKZ-5BFTS'
 
-#html = requests.get("https://aae7-2409-8a38-5810-42f0-54a1-8d8e-6ecf-7b53.ngrok-free.app")
-#print(html.text)
-requests.post(url='https://aae7-2409-8a38-5810-42f0-54a1-8d8e-6ecf-7b53.ngrok-free.app', data='aaaaaa')
+html = requests.get(url).text.encode('utf-8')
+res = json.loads(html)
+diction["name"] = name
+diction["location"] = [res["result"]['location']['lat'], res["result"]['location']['lng']]
+print(diction)
+requests.post(url='https://7081-2409-8a38-5810-42f0-c74-74c3-e816-e73a.ngrok-free.app', data=json.dumps(diction))
+class application(Tk):
+    def __init__(self, screenName: str | None = None, baseName: str | None = None, className: str = "Tk", useTk: bool = True, sync: bool = False, use: str | None = None) -> None:
+        super().__init__(screenName, baseName, className, useTk, sync, use)
+        
+if __name__ == "__main__":
+    app = application()
+    app.mainloop()
